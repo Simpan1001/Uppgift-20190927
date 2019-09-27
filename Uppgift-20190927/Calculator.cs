d1c84d1988f3edcdd224aa5a133d62764b95b4fb
Simpan1001 <simon.springer@edu.huddinge.se>
2019-09-27 15:24:57 +0200

    Added Add-function

---
+++ Uppgift-20190927/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_20190927
+{
+    class Calculator
+    {
+        //Denna metod ska ansvara för beräkningarna
+
+        //Egenskaper/Properties
+        //Metoder/Methods
+        //Add
+        public int Add(int number1, int number2) {
+            return number1 + number2;
+        }
+        //Subtract
+        //Divide
+        //Multiply
+        //Händelser/Events
+
+    }
+}
